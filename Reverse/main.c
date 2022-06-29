46be380100d1bc3d8b0436ab99a61d26e6625720
YN1CK <yannick.reiss@protonmail.ch>
2022-06-29 19:39:24 +0200

    Fixed Reverse-String function

--- Reverse/main.c
+++ Reverse/main.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
+// reverse: reverses a c-string
+// char* string: pointer to c-string to reverse
+// int length: Länge des strings in Zeichen (Das Nullbyte darf NICHT mitgezählt werden)
+// int i: als Startwert Null übergeben
 void reverse(char* string, int length, int i) {
     
     if (i < length - i) {
@@ -10,17 +15,15 @@ void reverse(char* string, int length, int i) {
 
         reverse(string, length,  i + 1);        
     }
-    /*for (int i = 0; i < (int)(length / 2); ++i) {
-
-    }*/
 }
 
 int main() {
 
-    char test[] = "Laufzeittest";
+    char test[] = "leohortetrohoel";
+    int length = sizeof(test) / sizeof(char);
 
-    reverse(test, 12, 0);
-    for (int i = 0; i < 13; ++i)
+    reverse(test, length - 1, 0);
+    for (int i = 0; i < 16; ++i)
         printf("%c", test[i]);
     printf("\n");
     return 0;
